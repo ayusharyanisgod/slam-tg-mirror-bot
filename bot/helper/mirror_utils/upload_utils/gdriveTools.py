a6b5244aee35100a571eff7904d8165517986c81
ayusharyanisgod <84650820+ayusharyanisgod@users.noreply.github.com>
2021-08-07 12:18:43 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -117,7 +117,7 @@ class GoogleDriveHelper:
                                      resumable=False)
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded using Slam Tg Mirror Bot',
+            'description': 'Uploaded By @TPVNetwork',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -171,7 +171,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Slam Tg Mirror Bot',
+            'description': 'Uploaded By @TPVNetwork',
             'mimeType': mime_type,
         }
         try:
